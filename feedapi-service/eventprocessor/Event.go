95a127cd609a7d200a26dfb91f0698f7b2aabbe4
jenspo <jens.pilemand.ottesen@vippsmobilepay.com>
2025-04-29 13:02:29 +0200

    Initial Commit of feedapi-service

---
+++ feedapi-service/eventprocessor/Event.go
@@ -0,0 +1,17 @@
+package eventprocessor
+
+import (
+	"github.com/goccy/go-json"
+	"time"
+)
+
+type Event struct {
+	SpecVersion     string          `json:"specVersion"`
+	DataContentType string          `json:"dataContentType"`
+	Type            string          `json:"type"`
+	Source          string          `json:"source"`
+	Subject         string          `json:"subject"`
+	ID              string          `json:"id"`
+	Time            time.Time       `json:"time"`
+	Data            json.RawMessage `json:"data"`
+}
